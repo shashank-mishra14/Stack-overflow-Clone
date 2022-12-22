d62ca788b4f20dc5e478f1fabdaa52603c799991
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2022-12-22 20:29:25 +0530

    Assests

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import  logo from '../../assets/logo.png'
+import  search from '../../assets/search.svg'
+import Avatar from '../../assets/Button/Button'
 const Navbar =()=>{
+
+    var User = null
 
 return(
     <navbar>    
@@ -9,10 +13,23 @@ return(
             <Link to ='/' className='nav-item nav-btn'> 
             <img src={logo} alt='logo'/>
              </Link>
-             <Link to ='/' className='nav-item nav-btn'></Link>
-              <Link to ='/' className='nav-item nav-btn'></Link>
-             <Link to ='/' className='nav-item nav-btn'></Link>
-            
+             <Link to ='/' className='nav-item nav-btn'>About</Link>
+              <Link to ='/' className='nav-item nav-btn'>Products</Link>
+             <Link to ='/' className='nav-item nav-btn'>For Teams</Link>
+             <form >
+                    <input type="text" placeholder= 'Search...'/>
+                    <img src={search} alt="search" width ="18"/>
+
+             </form>
+            { User === null ?
+            <Link to='/Auth' className='nav-items nav-links'>Log in</Link> : 
+            <>
+
+            <Link to='/'><Avatar>M</Avatar></Link>
+
+            <Button> Log out</Button>
+             </> 
+            }            
         </div>
     </navbar>
 
