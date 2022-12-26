38af952abbf0f361c6c2a60ab3565cbda10c0338
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2022-12-26 22:52:30 +0530

    Style

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -14,6 +14,30 @@ const Auth =()=> {
                     <div class='auth-container'>
                                 { !isSignup && <img src ={icon} alt='stack overflow' className='login-logo'/> }
                         <form>
+                                {
+
+                                        isSignup&&(
+                                                <label htmlFor='name'>
+                                                        <h4>Display Name</h4>
+                                                        <input type="text" id='name' name='name'/>
+                                                        {isSignup &&  <p style={{color:'#666767', fontSize:"13px"}}> Passwords must contains atleast eight <br/>characters, including atleast 1 letter and 1<br/> number
+                                                                </p>}
+                                                                {
+
+                                                                        isSignup &&(
+                                                                                <label htmlFor='check'>
+                                                                                        <input type='checkbox' id='check'/>
+                                                                                        <p style={{fontSize:"13px"}}>
+                                                                                           Opt-in to recieve occasional,<br/> product updates, use research invitations,<br/>
+                                                                                           company announcements , and digests
+                                                                                        </p>
+                                                                                        </label>
+                                                                        ) 
+                                                                }
+
+                                                </label>
+                                        )
+                                }
                         <label htmlFor="email">
                          <h4>Email</h4>
                          <input type="email" name='email' id='email'/>
@@ -28,6 +52,15 @@ const Auth =()=> {
                          <input type="password" name='password' id='password'/>
                           </label>
                           <button  type='submit' className='auth-btn'>{isSignup ? 'Sign up' :'Log in'}</button>
+                         {isSignup&&(
+                                <p style={{color:'#666767', fontSize:"13px"}}>
+                                        By clicking "Sign-up", you agree to our 
+                                        <span style={{color:'#007ac6'}}>terms of<br/> service</span>,
+                                        <span style={{color:'#007ac6'}}>privacy policy</span> and
+                                        <span style={{color:'#007ac6'}}>cookie policy</span>
+                                </p>
+                         )}
+
                         </form>
                         <p>
                            {isSignup ? 'Already have an account': "Don't have an acoount?"}
