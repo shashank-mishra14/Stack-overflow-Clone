f2b46a2591e51df47e9edbe3af35c93d56873ab2
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2022-12-27 23:58:21 +0530

    Widget

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import './RightSidebar.css'
+import Widget from './Widget'
+import WidgetTags from './WidgetTags'
+
 
 const RightSidebar = () => {
   return (
-    <div>
-
-        
-    </div>
+    <aside className='right-sidebar'>
+        <Widget/>
+        <WidgetTags/>
+    </aside>
   )
 }
 
---
+++ src/components/RightSidebar/WidgetTags.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WidgetTags = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default WidgetTags
