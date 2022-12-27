95136cfbad1aed8b688efb459d6659258b9ce079
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2022-12-27 22:52:13 +0530

    Sidebar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
+import HomeMainbar from "../../components/HomeMainbar/HomeMainbar";
 
 
  const Home =()=>{
 
     return(
 
- <div>
-<h1>
-   
-</h1>
-    
+ <div className='home-container-1'>
+    <LeftSidebar/>
+    <div className='home-container-2'>
+      <HomeMainbar/>
+    <RightSidebar/>
+
+    </div>
 
  </div>
     )
